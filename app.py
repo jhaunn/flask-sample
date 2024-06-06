6372def5da946a4f1053b373006fa197a051bcf8
jhaunn <89385585+jhaunn@users.noreply.github.com>
2024-06-06 05:56:10 +0000

    Added HTML, CSS, Dynamic Data and API Route

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'San Francisco, USA',
+        'salary': '$150,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Developer',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 12,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Remote',
+        'salary': '$100,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html',
+                          jobs = JOBS,
+                          webpage_name = 'Flask Webpage')
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port="8080")
+  app.run(host='0.0.0.0', port=8080)
